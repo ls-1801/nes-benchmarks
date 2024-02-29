901ef3c8fd25075b52f9776f21120be0af724999
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-02-29 19:32:39 +0100

    Update source cli params

--- run_source.sh
+++ run_source.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$NES_DIR/cmake-build-unikernel-release-original-deps/nes-unikernel-source/unikernel-source -c sources.yaml -n $1
+$NES_DIR/cmake-build-unikernel-release-original-deps/nes-unikernel-source/unikernel-source -c sources.yaml -n $1 -t DATA_FILE -f $(pwd)/bid.bin -s 1000000
 
